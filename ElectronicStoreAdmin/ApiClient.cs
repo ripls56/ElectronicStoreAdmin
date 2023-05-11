0bfcf0cdf056ab3644f61188e15b85f2aca6d5c2
ripls56 <84716344+ripls56@users.noreply.github.com>
2023-05-11 20:39:03 +0300

    Initial commit

---
+++ ElectronicStoreAdmin/ApiClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace ElectronicStoreAdmin
+{
+    class ApiClient
+    {
+        private const string BaseUrl = "https://localhost:7078/api/";
+
+        private static ApiClient _instance;
+        private static RestClient restClient;
+
+        private ApiClient()
+        { }
+
+        public static ApiClient getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ApiClient();
+                restClient = new RestClient();
+            }
+            return _instance;
+        }
+
+        public async Task<string?> GetAccessTokenAsync(string login, string password)
+        {
+            var request = new RestRequest(BaseUrl + "token/employee");
+            var body = new
+            {
+                login = login,
+                password = password
+            };
+            var bodySerialize = JsonConvert.SerializeObject(body);
+            request.AddBody(bodySerialize);
+            var response = await restClient.PostAsync(request).ConfigureAwait(false);
+            var deserialize = JObject.Parse(response.Content);
+            if ((int)(deserialize.GetValue("statusCode") ?? 1001) < 399)
+            {
+                return deserialize.GetValue("value")?["access_token"]?.ToString();
+            }
+
+            throw new HttpRequestException();
+
+        }
+    }
+}
---
+++ ElectronicStoreAdmin/Views/Pages/Authorization.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Wpf.Ui.Controls;
+
+namespace ElectronicStoreAdmin.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        private ApiClient apiClient;
+        public Authorization()
+        {
+            InitializeComponent();
+            apiClient = ApiClient.getInstance();
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                apiClient.GetAccessTokenAsync(Login.Text, Password.Password).GetAwaiter().GetResult();
+            }
+            catch
+            {
+                Snackbar.Show();
+            }
+            
+        }
+
+    }
+}
